65038b3bf55c01ff6ef249e6c20b521d1b18de2c
Awezome67 <azrielwmx@gmail.com>
2026-06-02 07:16:09 +0700

    update config2

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from '@prisma/config'
 import * as dotenv from 'dotenv'
 
-// Paksa agar variabel di file .env terbaca oleh sistem
 dotenv.config()
 
 export default defineConfig({
   datasource: {
-    url: process.env.DATABASE_URL,
-    // use shadowDatabaseUrl for migrations when using a separate database
-    shadowDatabaseUrl: process.env.DIRECT_URL,
+    // Paksa db push pakai DIRECT_URL di lokal agar tidak gantung/stuck
+    url: process.env.DIRECT_URL, 
   },
 })
