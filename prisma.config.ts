cdca475426e78fc2376a7221e9c3ab4557fcc5e1
Awezome67 <azrielwmx@gmail.com>
2026-06-02 07:24:25 +0700

    update config3

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,8 +4,12 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 export default defineConfig({
+  // Tambahkan bagian migrations di bawah ini:
+  migrations: {
+    // Gunakan 'ts-node ./prisma/seed.ts' jika kamu menggunakan npm/Node.js biasa
+    seed: 'npx tsx ./prisma/seed.ts', 
+  },
   datasource: {
-    // Paksa db push pakai DIRECT_URL di lokal agar tidak gantung/stuck
-    url: process.env.DIRECT_URL, 
+    url: process.env.DIRECT_URL,
   },
 })
